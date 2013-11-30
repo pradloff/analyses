77e1223b34b6639329c88adb6abfe8846f2554ac
pradloff <radloff@uoregon.edu>
2013-11-30 09:28:10 -0800

    added external maker

---
+++ make_externals.py
@@ -0,0 +1,23 @@
+from common.external import load
+
+if __name__=='__main__':
+
+	for package in [
+		'ApplyJetCalibration',
+		'ApplyJetResolutionSmearing',
+		'CalibrationDataInterface',
+		'ElectronEfficiencyCorrection',
+		'egammaAnalysisUtils',
+		'HSG4LepLepTriggerSF',
+		'JetResolution',
+		'JetUncertainties',
+		'JVFUncertaintyTool',
+		'MissingETUtility',
+		'MuonIsolationCorrection',
+		'MuonEfficiencyCorrections',
+		'MuonMomentumCorrections',
+		'PileupReweighting',
+		'TileTripReader',
+		'TrigMuonEfficiency',
+		]:
+		load(package,clean=True)
