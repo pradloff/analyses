7fdb0434d8a01a1d62a108dfd64fc4dfe75563df
pradloff <radloff@uoregon.edu>
2013-12-08 09:22:08 -0800

    verbose make-file output

--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-unset TMP
-unset TMPDIR
-
 export ANALYSISHOME=$DIR
 export ROOTCOREDIR=$DIR/external
 export PYTHONPATH=$ANALYSISHOME:$PYTHONPATH
