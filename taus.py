8a264dd2b83b7048e8a6f5cf7273ce53e4728a77
pradloff <radloff@uoregon.edu>
2013-12-11 11:13:16 -0800

    fix taus, set particle 4moment

--- taus.py
+++ taus.py
@@ -40,6 +40,9 @@ class collect_taus(event_function):
 				**dict((name,event.__dict__[self.collection_name+name][tau]) for name in self.names)
 				)
 
+		for tau in event.taus.values()
+			tau.set_pt_eta_phi_m(tau.Et,tau.eta,tau.phi,1777.)
+
 		#Define selections
 		for tau in event.taus.values():
 
