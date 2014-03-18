c64b124be5615983f46c6a7f7db2ad6b2a7f7e40
pradloff <radloff@uoregon.edu>
2014-03-17 22:30:48 -0700

    add mutated signal selection

--- selection.py
+++ selection.py
@@ -213,12 +213,32 @@ class mutate_make_selection_tt_control(analysis):
 			)
 
 
+class mutate_make_selection_signal(analysis):
+	def __init__(self):
+		analysis.__init__(self)
+		
+		self.add_event_function(
+			build_events(),
+			remove_overlapped_jets(),
+			compute_kinematics(),
+			get_weight(),
+			select_signal_events()
+			)
+
+		self.add_result_function(
+			plot_kinematics()
+			)
+
+		self.add_meta_result_function(
+			)
+
 class make_selection_signal(analysis):
 	def __init__(self):
 		analysis.__init__(self)
 		
 		self.add_event_function(
 			build_events(),
+			mutate_mumu_to_tautau(),
 			remove_overlapped_jets(),
 			compute_kinematics(),
 			get_weight(),
