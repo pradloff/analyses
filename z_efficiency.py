0486a168e77a559c8fc37948bf4607a7d6b413b5
pradloff <radloff@uoregon.edu>
2014-06-26 16:54:59 -0700

    add smearing error

--- z_efficiency.py
+++ z_efficiency.py
@@ -259,7 +259,10 @@ class efficiency_weight(event_function):
 		except KeyError: raise RuntimeError('Unknown lepton class {0}'.format(self.lepton_class))
 		self.efficiency_file = ROOT.TFile(file_name)
 		if not self.efficiency_file: raise RuntimeError('Unknown file {0}'.format(file_name))
-
+		for name in sorted([key.GetName() for key in self.efficiency_file.GetListOfKeys()]):
+			if 'resolution' not in name: continue
+			self.efficiency_file.Get(name).SetErrorOption('s')
+			
 class inefficiency_weight(event_function):
 	def __init__(self,lepton_class=arg(int,required=True,help='{0:ee,1:mumu,2:emu}')):
 		event_function.__init__(self)
@@ -289,7 +292,10 @@ class inefficiency_weight(event_function):
 		except KeyError: raise RuntimeError('Unknown lepton class {0}'.format(self.lepton_class))
 		self.efficiency_file = ROOT.TFile(file_name)
 		if not self.efficiency_file: raise RuntimeError('Unknown file {0}'.format(file_name))
-
+		for name in sorted([key.GetName() for key in self.efficiency_file.GetListOfKeys()]):
+			if 'resolution' not in name: continue
+			self.efficiency_file.Get(name).SetErrorOption('s')
+			
 class plot_kinematics_offline(result_function):
 	def __init__(self):
 		result_function.__init__(self)
