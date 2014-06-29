4a68e0ed51b45af72d3b00d90b47f43ec15f6673
pradloff <radloff@uoregon.edu>
2014-06-28 21:38:40 -0700

    smear truth

--- z_efficiency.py
+++ z_efficiency.py
@@ -327,7 +327,7 @@ class smear(event_function):
 		self.initialize_tools()
 
 	def __call__(self,event):
-		if event.l1_offline_pt < event.l2_offline_pt: event.l1_offline,event.l2_offline = event.l2_offline,event.l1_offline
+		if event.l1.pt < event.l2.pt: event.l1,event.l2 = event.l2,event.l1
 
 		for particle,hist in [
 			(event.l1,self.efficiency_file.l1_pt_resolution),
