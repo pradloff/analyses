6a734634e0bace16e6be7380671164263b154fbb
Peter <radloff@uoregon.edu>
2014-07-31 15:19:36 -0700

    emu debugging

--- z_efficiency2.py
+++ z_efficiency2.py
@@ -523,8 +523,9 @@ class efficiency_weight(event_function):
 			event.l1,event.l2 = event.l2,event.l1
 
 		
-		efficiency = get_selection_efficiency(self.efficiency_file,event.l1.eta,event.l2.eta,event.l1.pt,event.l2.pt)
+		efficiency = get_selection_efficiency(self.efficiency_file,event.l1.eta,event.l2.eta,event.l1.pt,event.l2.pt,debug=True)
 		if efficiency < 0.:
+			print efficiency, event.l1.eta, event.l2.eta, event.l1.pt, event.l2.pt
 			event.__break__ = True
 			return
 
