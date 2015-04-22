72d9b449b035164148c4e891d3a866c072d17714
pradloff <radloff@uoregon.edu>
2015-04-22 08:51:33 -0500

    selection

--- selection.py
+++ selection.py
@@ -1537,7 +1537,7 @@ class jes_uncertainty(event_function):
         )
     def __init__(self,component=None):
         super(jes_uncertainty,self).__init__()
-        self.component
+        self.component = component
     def setup():
         load("JetUncertainties")
         self.jes = ROOT.JESUncertaintyProvider(
