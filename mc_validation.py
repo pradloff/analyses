314fcf33e00cf5d05e279369a16ea24957d4b6f0
peter@8bitstudio.com <peter@8bitstudio.com>
2016-03-29 10:04:11 -0500

    update mc_validation with phi pT range select

--- mc_validation.py
+++ mc_validation.py
@@ -80,9 +80,9 @@ class identify_pythia_truth(event_function):
 			'f1',
 			'f2',
 			]:
-        	self.branches += [
-            	auto_branch(prefix'_'+name,'w','Float_t') for name in self.names
-            	]
+			self.branches += [
+				auto_branch(prefix'_'+name,'w','Float_t') for name in self.names
+				]
 
 	def __call__(self,event):
 		super(identify_pythia_truth,self).__call__(event)
